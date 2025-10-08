07a33c3e4d3a6947a408fe69845c802e4e44aaa6
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-10-08 13:28:11 +0100

    add cloudflare configuration for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,32 @@ export default defineNuxtConfig({
   nitro: {
     experimental: {
       openAPI: true
+    },
+    preset: 'cloudflare_module',
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+      wrangler: {
+        name: 'room-bookings',
+        routes: [
+          {
+            pattern: 'rooms.newtheatre.org.uk',
+            custom_domain: true
+          }
+        ],
+        d1_databases: [
+          {
+            binding: 'DB',
+            database_name: 'rooms',
+            database_id: '820d8e64-108d-4604-a453-d78595a1e1ef'
+          }
+        ],
+        observability: {
+          logs: {
+            enabled: true
+          }
+        }
+      }
     }
   },
 
